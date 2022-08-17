762659e96448323c4c9591c51b5ea9a20a35757e
Yuriy Voshchepynets <yuriy.voshch@gmail.com>
2022-08-17 17:14:13 +0200

    test new timeout support

--- examples/redis.rs
+++ examples/redis.rs
@@ -14,6 +14,22 @@ fn fetch_an_integer() -> redis::RedisResult<isize> {
     con.get("my_key")
 }
 
+fn push_queue(value: u32) -> redis::RedisResult<isize> {
+    // connect to redis
+    let client = redis::Client::open("redis://127.0.0.1/")?;
+    let mut con = client.get_connection()?;
+    // throw away the result, just make sure it does not fail
+    con.rpush("my_queue", value)
+}
+
+fn poll_value() -> redis::RedisResult<isize> {
+    // connect to redis
+    let client = redis::Client::open("redis://127.0.0.1/")?;
+    let mut con = client.get_connection()?;
+    // timeout for two seconds
+    con.blpop("my_queue", 2)
+}
+
 #[lunatic::main]
 fn main(_: Mailbox<()>) {
     println!("MAIN");
@@ -24,4 +40,11 @@ fn main(_: Mailbox<()>) {
     );
     let (_, my_key) = proc.receive();
     println!("Fetched my_key from redis {:?}", my_key);
+
+    // try to pop without any values
+    println!("POLLING FIRST {:?}", poll_value());
+
+    // push the pop
+    println!("PUSHING INTO QUEUE {:?}", push_queue(55));
+    println!("POLLING 55 {:?}", poll_value());
 }
